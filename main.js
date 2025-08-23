6091412c56d699a94554786ecf35f23baf213a68
sunnyonweb <sunnytalukder222@gmail.com>
2025-08-23 18:14:54 +0600

    Login form complete

---
+++ main.js
@@ -0,0 +1,16 @@
+// login button function
+document.getElementById('login-btn').addEventListener('click', function(e) {
+    e.preventDefault()
+    const mobaileNumber = '01643963952' ;
+    const pinNumber = '0000' ;
+    const mobaileNumberValue = document.getElementById('mobail').value ;
+
+    const pinNumberValue = document.getElementById('pin').value ;
+
+    if( mobaileNumberValue=== mobaileNumber && pinNumberValue === pinNumber){
+        window.location.href = './home.html'
+    }
+    else{
+        alert("Invalid Number or Pin!");
+    }
+});
